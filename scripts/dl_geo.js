1d4eb5353acff4840cab466bd44f5c1c36d30a57
Clavin <leezj1989@gmail.com>
2023-06-12 15:46:19 +0800

    Update geoip and geosite url

--- scripts/dl_geo.js
+++ scripts/dl_geo.js
@@ -2,10 +2,30 @@ const axios = require('axios').default
 const path = require('path')
 const os = require('os')
 const fs = require('fs')
-const targz = require('targz')
 
-const geositeUrl = 'https://github.com/v2ray/domain-list-community/releases/latest/download/dlc.dat'
-const mmdbUrl = 'https://web.archive.org/web/20191227182412/https://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz'
+const geoipUrl = 'https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat'
+const geositeUrl = 'https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat'
+
+async function downloadGeoIP() { 
+  const dstPath = path.join(__dirname, '../src/helper', 'geoip.dat')
+  const writer = fs.createWriteStream(dstPath)
+  console.log('Downloading', geoipUrl)
+  const resp = await axios({
+    url: geoipUrl,
+    method: 'GET',
+    responseType: 'stream',
+    onDownloadProgress: (e) => {
+      console.log(e)
+    }
+  })
+  resp.data.pipe(writer)
+  writer.on('finish', () => {
+    console.log('Saved file to', dstPath)
+  })
+  writer.on('error', (err) => {
+    console.log('Download geoip.dat failed.', err)
+  })
+}
 
 async function downloadGeosite() { 
   const dstPath = path.join(__dirname, '../src/helper', 'geosite.dat')
@@ -21,7 +41,7 @@ async function downloadGeosite() {
   })
   resp.data.pipe(writer)
   writer.on('finish', () => {
-    console.log('Saved file', dstPath)
+    console.log('Saved file to', dstPath)
   })
   writer.on('error', (err) => {
     console.log('Download geosite.dat failed.', err)
@@ -45,51 +65,5 @@ function findInDir (dir, filter, fileList = []) {
   return fileList;
 }
 
-async function downloadGeommdb() {
-  const tmpFolder = path.join(os.tmpdir(), 'mellow')
-  if (!fs.existsSync(tmpFolder)) {
-    fs.mkdirSync(tmpFolder)
-  }
-  const dlname = 'mmdb.tar.gz'
-  const tarPath = path.join(tmpFolder, dlname)
-  const writer = fs.createWriteStream(tarPath)
-  console.log('Downloading', mmdbUrl) 
-  const resp = await axios({
-    url: mmdbUrl,
-    method: 'GET',
-    responseType: 'stream',
-    onDownloadProgress: (e) => {
-      console.log(e)
-    }
-  })
-  resp.data.pipe(writer)
-  writer.on('finish', () => {
-    console.log('Saved file', tarPath)
-    targz.decompress({
-      src: tarPath,
-      dest: tmpFolder
-    }, (err) => {
-      if (err) {
-        console.log('Failed to extract mmdb.', err)
-        process.exit(1)
-      } else {
-        const dstPath = path.join(__dirname, '../src/helper', 'geo.mmdb')
-        const fileName = 'GeoLite2-Country.mmdb'
-        const files = findInDir(tmpFolder, /\.mmdb/)
-        if (files.length != 1) {
-          console.log('mmdb file not found.')
-          process.exit(1)
-        }
-        fs.renameSync(files[0], dstPath)
-        console.log('Saved file', dstPath)
-      }
-    })
-
-  })
-  writer.on('error', (err) => {
-    console.log('Download Geo mmdb failed.', err)
-  })
-}
-
+downloadGeoIP()
 downloadGeosite()
-downloadGeommdb()
